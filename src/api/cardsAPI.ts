252524cbef8bb367e669da17f53385e21c7326d5
unknown <you@example.com>
2022-09-09 14:58:16 +0300

    fix:

--- src/api/cardsAPI.ts
+++ src/api/cardsAPI.ts
@@ -3,7 +3,7 @@ import {ForgotPasswordParamAC, RegisterParamAC} from "../auth/auth-reducer";
 
 
 export const instance = axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ?  'http://localhost:7542/2.0/' : 'https://neko-back.herokuapp.com/2.0' ,
+    baseURL:'https://neko-back.herokuapp.com/2.0/',
     withCredentials: true,
 })
 
